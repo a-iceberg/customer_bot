f6c1b2f0a05a54b553d34c5977005432dc548ee0
Alex <format37@gmail.com>
2024-04-05 13:59:26 +0400

    modified:   bot.py

--- bot.py
+++ bot.py
@@ -52,7 +52,7 @@ def create_location_tool(bot, chat_id):
         func=send_location_request,
         name="Request Location",
         description="Используй разово, когда тебе нужно запросить локацию пользователя для дальнейшего использования этой информации при создании заявки, чтобы помочь пользователю.",
-        return_direct=True,
+        return_direct=False,
     )
 
 
@@ -72,7 +72,7 @@ def create_contact_tool(bot, chat_id):
         func=send_contact_request,
         name="Request Contact",
         description="Используй разово, когда тебе нужно запросить контакты пользователя для дальнейшего использования этой информации при создании заявки, чтобы помочь пользователю.",
-        return_direct=True,
+        return_direct=False,
     )
 
 
