a5ed2fc03343ee8df9599bc0f7c23154f9b5f469
Vladislav Nesterov <darveivoldavara@gmail.com>
2024-04-08 14:14:10 +0300

    added saving chat history in file

--- bot.py
+++ bot.py
@@ -4,12 +4,13 @@ import logging
 import telebot
 
 from langchain_community.llms import Ollama
-from langchain.chains import LLMChain
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
 
 from langchain.agents import initialize_agent, AgentType
 from langchain.tools.base import StructuredTool
+
+from config_manager import ConfigManager
+from chat_history_service import ChatHistoryService
 
 app = FastAPI()
 
@@ -20,96 +21,80 @@ llm = Ollama(
     model="command-r",
     base_url="http://10.2.4.87:11434",
     keep_alive=-1,
-    system="Ты - сотрудник колл-центра сервисного центра по ремонту бытовой техники. Отвечай на сообщения и вопросы пользователя максимально дружелюбно. Твоя основная цель - получить у пользователя его локацию и телефон для последующего создания заявки. Тебе доступен набор инструментов. Настоятельно рекомендуется их использовать для выполнения основной цели.",
-    num_ctx=2048,
-    repeat_last_n=-1,
+    system="Ты - сотрудник колл-центра сервисного центра по ремонту бытовой техники. Отвечай на сообщения и вопросы пользователя максимально дружелюбно. Твоя основная итоговая цель - получить у пользователя его локацию и телефон для последующего создания заявки и отвечать сообщениями на сообщения пользователя. Тебе доступен набор инструментов. Настоятельно рекомендуется их использовать для выполнения основной цели. Отвечай на русском языке, учитывая контекст переписки.",
+    num_ctx=8192,
+    repeat_last_n=4096,
     temperature=0.3,
 )
 
-# prompt = ChatPromptTemplate.from_messages(
-#     [
-#         MessagesPlaceholder("chat_history"),
-#         ("user", "{input}"),
-#     ]
-# )
-# llm_chain = LLMChain(llm=llm, prompt=prompt)
-
+config_manager = ConfigManager("config.json")
+chat_history_service = ChatHistoryService(config_manager.get("chats_dir"), logger)
 
 def create_location_tool(bot, chat_id):
     def send_location_request():
         keyboard = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
         button = telebot.types.KeyboardButton(
-            text="Send your location", request_location=True
+            text="Ваше местоположение", request_location=True
         )
         keyboard.add(button)
         bot.send_message(
-            chat_id, "Please submit your location below", reply_markup=keyboard
+            chat_id,
+            "Укажите ваше местоположение через кнопку ниже",
+            reply_markup=keyboard,
         )
-        # return "Локация пользователя была успешно запрошена"
-        # return 'Локация пользователя была успешно запрошена. Ответь пожалуйста пользователю: "Пожалуйста, отправьте свои координаты, нажав кнопку внизу."'
-        return 'Location was sent'
+        return "Локация пользователя была успешно запрошена"
 
     return StructuredTool.from_function(
         func=send_location_request,
         name="Request Location",
-        description="Используй разово, когда тебе нужно запросить локацию пользователя для дальнейшего использования этой информации при создании заявки, чтобы помочь пользователю.",
-        return_direct=True,
+        description="Используй, когда тебе нужно запросить локацию пользователя для дальнейшего использования этой информации при создании заявки, чтобы помочь пользователю.",
+        return_direct=False,
     )
 
 
 def create_contact_tool(bot, chat_id):
     def send_contact_request():
         keyboard = telebot.types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-        button = telebot.types.KeyboardButton(
-            text="Send your contact", request_contact=True
-        )
+        button = telebot.types.KeyboardButton(text="Ваш телефон", request_contact=True)
         keyboard.add(button)
         bot.send_message(
-            chat_id, "Please submit your contact below", reply_markup=keyboard
+            chat_id, "Укажите ваш телефон через кнопку ниже", reply_markup=keyboard
         )
-        # return 'Контакты пользователя были успешно запрошены. Ответь пожалуйста пользователю: "Пожалуйста, отправьте свои контактные данные, нажав кнопку внизу."'
-        return 'Contacts was sent'
+        return "Контакты пользователя были успешно запрошены"
 
     return StructuredTool.from_function(
         func=send_contact_request,
         name="Request Contact",
-        description="Используй разово, когда тебе нужно запросить контакты пользователя для дальнейшего использования этой информации при создании заявки, чтобы помочь пользователю.",
-        return_direct=True,
+        description="Используй, когда тебе нужно запросить контакты пользователя для дальнейшего использования этой информации при создании заявки, чтобы помочь пользователю.",
+        return_direct=False,
     )
 
-
-chat_history = {}
 
 @app.post("/message")
 async def call_message(request: Request, authorization: str = Header(None)):
     logger.info("post: message")
-    empty_response = JSONResponse(content={"type": "empty", "body": ""})
+
     token = None
     if authorization and authorization.startswith("Bearer "):
         token = authorization.split(" ")[1]
 
     else:
-        answer = "Failed to get bot token"
+        answer = "Не удалось получить токен бота"
         return JSONResponse(content={"type": "text", "body": str(answer)})
 
     if token:
         message = await request.json()
-        logger.info(f"Message: {message}")
+
         bot = telebot.TeleBot(token)
 
         chat_id = message["chat"]["id"]
+
         if "text" not in message:
-            return empty_response
+            return JSONResponse(content={"type": "empty", "body": ""})
+
         user_message = message["text"]
 
-        if user_message == "/reset":
-            chat_history[chat_id] = []
-            bot.send_message(chat_id, "Chat history has been reset")
-
-        if chat_id not in chat_history:
-            chat_history[chat_id] = []
-
-        if user_message != "/start" and user_message != "/reset":
+        if user_message != "/start":
             location_tool = create_location_tool(bot, chat_id)
             contact_tool = create_contact_tool(bot, chat_id)
             tools = [location_tool, contact_tool]
@@ -121,51 +106,33 @@ async def call_message(request: Request, authorization: str = Header(None)):
                 verbose=True,
                 handle_parsing_errors=True,
             )
-            # keyboard = telebot.types.ReplyKeyboardMarkup(
-            #     row_width=1, resize_keyboard=True
-            # )
-            # button = telebot.types.KeyboardButton(
-            #     text="Send your location",
-            #     request_location=True,
-            # )
-            # keyboard.add(button)
 
-            # bot.send_message(
-            #     chat_id, "Please submit your location below", reply_markup=keyboard
-            # )
+            message_text = f"Ты - сотрудник колл-центра сервисного центра по ремонту бытовой техники. Отвечай на сообщения и вопросы пользователя максимально дружелюбно. Твоя основная итоговая цель - получить у пользователя его локацию и телефон для последующего создания заявки и отвечать сообщениями на сообщения пользователя. Тебе доступен набор инструментов. Настоятельно рекомендуется их использовать для выполнения основной цели. Отвечай на русском языке, учитывая контекст переписки. Сейчас ты получил следующее сообщение: {user_message}"
 
             try:
-                # bot_response = llm_chain.invoke(
-                #     {"chat_history": chat_history[chat_id], "input": user_message}
-                # )["text"]
-                
-                # anquet = {"Location":{"latitude":0,"longitude":0},"Contact":{"phone_number":""}}
-                # user_message = f"Anquet: {str(anquet)}\nUser message:{user_message}"
+                await chat_history_service.save_to_chat_history(
+                    chat_id,
+                    user_message,
+                    message["message_id"],
+                    "HumanMessage",
+                    message["from"]["first_name"],
+                    "human",
+                )
 
-                bot_response = agent.run(
-                    input=user_message, chat_history=chat_history[chat_id]
+                chat_history = await chat_history_service.read_chat_history(chat_id)
+                bot_response = agent.run(input=message_text, chat_history=chat_history)
+
+                await chat_history_service.save_to_chat_history(
+                    chat_id,
+                    bot_response,
+                    message["message_id"],
+                    "AIMessage",
+                    message["from"]["first_name"],
+                    "llm",
                 )
-                chat_history[chat_id].extend(
-                    [
-                        HumanMessage(content=user_message),
-                        AIMessage(content=bot_response),
-                    ]
-                )
+
                 logger.info(f"History for {chat_id}: {chat_history[chat_id]}")
-                if bot_response == 'Location was sent' \
-                    or bot_response == 'Contacts was sent':                    
-                    return empty_response
-                else:
-                    # bot.send_message(chat_id, bot_response)
-                    return JSONResponse(content={"type": "text", "body": bot_response})
+                bot.send_message(chat_id, bot_response)
 
             except Exception as e:
                 logger.info(f"Error: {e}")
-
-            # chat_history[chat_id].extend(
-            #     [HumanMessage(content=user_message), AIMessage(content=bot_response)]
-            # )
-            # logger.info(f"History for {chat_id}: {chat_history[chat_id]}")
-
-            # bot.send_message(chat_id, bot_response)
-            return empty_response
