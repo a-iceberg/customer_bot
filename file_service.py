001b5e7dec51ea49568449e3490113b713ce2819
Vladislav Nesterov <darveivoldavara@gmail.com>
2024-04-25 18:30:56 +0300

    modified:   file_service.py

--- file_service.py
+++ file_service.py
@@ -149,8 +149,8 @@ class FileService:
             try:
                 with open(full_path, "r") as file:
                     message = json.load(file)
-                    if message["type"] == "category":
-                        request_items["category"] = message["text"]
+                    if message["type"] == "direction":
+                        request_items["direction"] = message["text"]
                     elif message["type"] == "phone":
                         request_items["phone"] = message["text"]
                     elif message["type"] == "latitude":
