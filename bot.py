4846390e996967a236c87f5966d31da97469a927
Vladislav <darveivoldavara@gmail.com>
2024-08-13 11:14:20 +0000

    Fixed message id

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ class Application:
             self.logger
         )
         self.channel_manager = ConfigManager(
-            "./data/cc/channel_posts.json",
+            "./data/vlad/channel_posts.json",
             self.logger
         )
         self.coordinates_manager = ConfigManager(
