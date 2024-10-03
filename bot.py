618f59d90a144f1380ea7b2b1f2cd56f1da6c441
Vladislav <darveivoldavara@gmail.com>
2024-10-03 14:13:25 +0000

    modified:   data/config.json

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ class Application:
             self.logger
         )
         self.channel_manager = ConfigManager(
-            "./data/vlad/channel_posts.json",
+            "./data/сс/channel_posts.json",
             self.logger
         )
         self.coordinates_manager = ConfigManager(
